39162e7f179646102d64f24368f5bc666e5d1643
pip <philip.monheim@stud.h-da.de>
2020-02-02 16:51:51 +0100

    1 Encoder Working!!!
    Olive Sends and Receives OSC Messages,
    LEDring remotecontrolled!

--- AltController/Olive/Olive.ino
+++ AltController/Olive/Olive.ino
@@ -29,8 +29,30 @@ Adafruit_NeoPixel *LEDring;
 Encoder enc(0, 1);
 // Adafruit_NeoPixel LEDring = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ400);
 
-int rvalue = 1;
+int ledValue = 1;
 int pixelcount = 8;
+
+void EncoderOne () {
+  long newEnc;
+  newEnc = enc.read()*-1;                 // Encoders wired wrong way 'round...
+  if (newEnc != 0) {
+    //if (newEnc < 0 && ledValue < 42) { rvalue++; }          
+    //else if (ledValue > 0 && ledValue > 0) { rvalue--; }        
+    //else if (ledValue < 0) { ledValue = 0; }                 //returns rvalue back to 0 if negative
+    //else if (ledValue > 42) { ledValue = 42; }               //returns rvalue to 21 if overshot
+    //Serial.print("Ecoder Value: ");
+    //Serial.println(newEnc);
+    //Serial.print("rvalue: ");
+    //Serial.println(ledValue);
+    enc.write(0);
+    OSCMessage OutMsg("/track/enc/2/svalue");
+    OutMsg.add(newEnc);
+    SLIPSerial.beginPacket();  
+    OutMsg.send(SLIPSerial); // send the bytes to the SLIP stream
+    SLIPSerial.endPacket(); // mark the end of the OSC Packet
+    OutMsg.empty(); // free space occupied by message
+  }
+}
 
 void LEDTest()
 {
@@ -66,11 +88,12 @@ void LEDcontrol(OSCMessage &msg)
 {
     if (msg.isInt(0))
     {
-         rvalue = msg.getInt(0);
+         ledValue = msg.getInt(0);
+         msg.empty();
     }
     
-  int fullLED = rvalue / 6;  //calculate fully lit LEDs
-  int restLED = rvalue % 6;  //Calculate value for partially lit LED
+  int fullLED = ledValue / 6;  //calculate fully lit LEDs
+  int restLED = ledValue % 6;  //Calculate value for partially lit LED
   for (uint16_t i=0;i<pixelcount;i++)  //Cycle through all LEDs of a LED Ring
   {
     if (i <= fullLED) //Cycle through all fully lit LEDs
@@ -91,7 +114,7 @@ void LEDcontrol(OSCMessage &msg)
 }
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   SLIPSerial.begin(9600);   // set this as high as you can reliably run on your platform
   LEDring = new Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
   LEDring->begin();
@@ -101,8 +124,8 @@ void setup() {
 void loop() {
 
 /* // for internal control of LEDRing by Encoder only. Defunct when using OSC
-  int fullLED = rvalue / 6;  //calculate fully lit LEDs
-  int restLED = rvalue % 6;  //Calculate value for partially lit LED
+  int fullLED = ledValue / 6;  //calculate fully lit LEDs
+  int restLED = ledValue % 6;  //Calculate value for partially lit LED
   for (uint16_t i=0;i<pixelcount;i++)  //Cycle through all LEDs of a LED Ring
   {
     if (i <= fullLED) //Cycle through all fully lit LEDs
@@ -121,33 +144,32 @@ void loop() {
   }
  */ 
 
-  /*OSCBundle bundleIN;
+  OSCBundle InBundle;
   int size;
-
-  while(!SLIPSerial.endofPacket())
+  while(!SLIPSerial.endofPacket()) {
+    EncoderOne();
     if( (size =SLIPSerial.available()) > 0)
     {
        while(size--)
-          bundleIN.fill(SLIPSerial.read());
+       InBundle.fill(SLIPSerial.read());
      }
-  
-  if(!bundleIN.hasError())
+  if(!InBundle.hasError())
   {
-   bundleIN.dispatch("/track/enc/2/rvalue", LEDcontrol);
-  }*/
-
+   InBundle.dispatch("/track/enc/2/rvalue", LEDcontrol);
+  }
+  }
   
-  long newEnc;
+  /*long newEnc;
   newEnc = enc.read()*-1;                 // Encoders wired wrong way 'round...
   if (newEnc != 0) {
-    //if (newEnc < 0 && rvalue < 42) { rvalue++; }          
-    //else if (newEnc > 0 && rvalue > 0) { rvalue--; }        
-    //else if (rvalue < 0) { rvalue = 0; }                 //returns rvalue back to 0 if negative
-    //else if (rvalue > 42) { rvalue = 42; }               //returns rvalue to 21 if overshot
-    Serial.print("Ecoder Value: ");
-    Serial.println(newEnc);
-    Serial.print("rvalue: ");
-    Serial.println(rvalue);
+    //if (newEnc < 0 && ledValue < 42) { rvalue++; }          
+    //else if (ledValue > 0 && ledValue > 0) { rvalue--; }        
+    //else if (ledValue < 0) { ledValue = 0; }                 //returns rvalue back to 0 if negative
+    //else if (ledValue > 42) { ledValue = 42; }               //returns rvalue to 21 if overshot
+    //Serial.print("Ecoder Value: ");
+    //Serial.println(newEnc);
+    //Serial.print("rvalue: ");
+    //Serial.println(ledValue);
     enc.write(0);
     OSCMessage OutMsg("/track/enc/2/svalue");
     OutMsg.add(newEnc);
@@ -155,7 +177,6 @@ void loop() {
     OutMsg.send(SLIPSerial); // send the bytes to the SLIP stream
     SLIPSerial.endPacket(); // mark the end of the OSC Packet
     OutMsg.empty(); // free space occupied by message
-    delay(20);
-  }
+  }*/
  
 }
